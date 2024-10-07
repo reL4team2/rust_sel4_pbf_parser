36aa55d67b2b88b225d5ed5b5aabed62b0dc9b03
ZhiyuanSue <2262387848@qq.com>
2024-10-07 16:53:17 +0800

    update the pub

--- src/parser.rs
+++ src/parser.rs
@@ -5,7 +5,7 @@ use glob::glob;
 use super::bf;
 use proc_macro2::TokenStream;
 use sel4_rustfmt_helper::Rustfmt;
-fn pbf_parser(in_dir:String, out_dir:String) {
+pub fn pbf_parser(in_dir:String, out_dir:String) {
 	let out_dir = OutDir::new(out_dir);
 
 	let mut blocklist_for_bindgen = vec![];

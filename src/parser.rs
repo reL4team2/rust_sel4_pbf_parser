15d4354862a068a14c694a301e2e1d939288f582
ZhiyuanSue <2262387848@qq.com>
2024-10-07 17:23:13 +0800

    update the output

--- src/parser.rs
+++ src/parser.rs
@@ -9,7 +9,6 @@ pub fn pbf_parser(in_dir:String, out_dir:String) {
 	let out_dir = OutDir::new(out_dir);
 
 	let mut blocklist_for_bindgen = vec![];
-    println!("start generate the rs,current path is {}!",env::current_dir().unwrap().display());
 	for f in glob(&format!("{}/*.pbf",in_dir))
 		.unwrap()
 		.map(Result::unwrap)
@@ -38,7 +37,6 @@ impl OutDir {
 
     fn write_file(&self, fragment: TokenStream, filename: impl AsRef<Path>) {
         let out_path = self.path.join(filename);
-		println!("out_path is {}",out_path.display());
         fs::write(&out_path, format!("{fragment}")).unwrap();
         self.rustfmt.format(&out_path);
     }
